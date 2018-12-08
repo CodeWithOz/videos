c0f0e750937e5ed7a10badfcd94b3ccf3bff4c0e
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-08 13:22:35 +0100

    Update state with search results

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
@@ -10,7 +12,7 @@ class App extends React.Component {
       }
     });
 
-    console.log(response);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
