1adee4d9423ef5b0129e5bed31ffc18204f169c2
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-09 05:35:34 +0100

    Get API key from an ignored file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-
-const KEY = 'AIzaSyA7ZeuBlPudAGPcIg1b-T2lL96rijs5ApY';
+import secrets from '../secrets';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: KEY
+    key: secrets.YOUTUBE_API_KEY
   }
 });
